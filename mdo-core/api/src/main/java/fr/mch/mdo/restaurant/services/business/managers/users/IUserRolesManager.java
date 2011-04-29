74e10696d3d40b4c2c34c2ac1a23644dc8900203
Mathieu MA <mathieu.ma.dev@free.fr>
2011-04-29 23:52:25 +0000

    Initial commit from Ubuntu

--- mdo-core/api/src/main/java/fr/mch/mdo/restaurant/services/business/managers/users/IUserRolesManager.java
+++ mdo-core/api/src/main/java/fr/mch/mdo/restaurant/services/business/managers/users/IUserRolesManager.java
@@ -1,7 +1,12 @@
 package fr.mch.mdo.restaurant.services.business.managers.users;
 
+import fr.mch.mdo.restaurant.beans.IMdoDtoBean;
+import fr.mch.mdo.restaurant.dto.beans.MdoUserContext;
+import fr.mch.mdo.restaurant.exception.MdoException;
 import fr.mch.mdo.restaurant.services.business.managers.IAdministrationManager;
+import fr.mch.mdo.restaurant.services.business.managers.IManagerLabelable;
 
-public interface IUserRolesManager extends IAdministrationManager
+public interface IUserRolesManager extends IAdministrationManager, IManagerLabelable
 {
+	IMdoDtoBean findByCode(String code, MdoUserContext userContext) throws MdoException; 
 }
