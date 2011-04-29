15d728000524eb4978494117786235c62c94be36
Mathieu MA <mathieu.ma.dev@free.fr>
2011-04-29 23:51:20 +0000

    Initial commit from Ubuntu

---
+++ mdo-core/api/src/main/java/fr/mch/mdo/restaurant/services/business/managers/printings/IPrintingInformationsManager.java
@@ -0,0 +1,26 @@
+package fr.mch.mdo.restaurant.services.business.managers.printings;
+
+import java.util.List;
+import java.util.Map;
+
+import fr.mch.mdo.restaurant.beans.IMdoDtoBean;
+import fr.mch.mdo.restaurant.dto.beans.LocaleDto;
+import fr.mch.mdo.restaurant.dto.beans.MdoUserContext;
+import fr.mch.mdo.restaurant.exception.MdoException;
+import fr.mch.mdo.restaurant.services.business.managers.IAdministrationManager;
+
+
+public interface IPrintingInformationsManager extends IAdministrationManager
+{
+	Map<Long, String> getLabels(LocaleDto currentLocale) throws MdoException;
+	
+	/**
+	 * Get list a list of printing informations by restaurant id.
+	 * @param restaurantId the restaurant id.
+	 * @param userContext the user context.
+	 * @return a list of printing informations
+	 * @throws MdoException when any exception occur.
+	 */
+	List<IMdoDtoBean> getList(Long restaurantId, MdoUserContext userContext) throws MdoException;
+
+}
