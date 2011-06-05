9037700a033f37da4a57ddb41daaf1e3a29328ec
Mathieu MA <mathieu.ma.dev@free.fr>
2011-06-05 21:25:37 +0000

    Initial commit from Ubuntu

--- mdo-core/business/src/main/java/fr/mch/mdo/restaurant/services/business/managers/products/DefaultProductsManager.java
+++ mdo-core/business/src/main/java/fr/mch/mdo/restaurant/services/business/managers/products/DefaultProductsManager.java
@@ -1,7 +1,27 @@
 package fr.mch.mdo.restaurant.services.business.managers.products;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import org.jopendocument.dom.spreadsheet.Sheet;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
 
 import fr.mch.mdo.logs.ILogger;
 import fr.mch.mdo.restaurant.beans.IBeanLabelable;
@@ -9,11 +29,16 @@ import fr.mch.mdo.restaurant.beans.IMdoBean;
 import fr.mch.mdo.restaurant.beans.IMdoDaoBean;
 import fr.mch.mdo.restaurant.beans.IMdoDtoBean;
 import fr.mch.mdo.restaurant.dao.beans.Product;
+import fr.mch.mdo.restaurant.dao.beans.ProductCategory;
 import fr.mch.mdo.restaurant.dao.products.IProductsDao;
 import fr.mch.mdo.restaurant.dao.products.hibernate.DefaultProductsDao;
 import fr.mch.mdo.restaurant.dto.beans.IAdministrationManagerViewBean;
+import fr.mch.mdo.restaurant.dto.beans.LocaleDto;
 import fr.mch.mdo.restaurant.dto.beans.MdoUserContext;
+import fr.mch.mdo.restaurant.dto.beans.ProductDto;
 import fr.mch.mdo.restaurant.dto.beans.ProductsManagerViewBean;
+import fr.mch.mdo.restaurant.dto.beans.RestaurantDto;
+import fr.mch.mdo.restaurant.dto.beans.ValueAddedTaxDto;
 import fr.mch.mdo.restaurant.exception.MdoBusinessException;
 import fr.mch.mdo.restaurant.exception.MdoException;
 import fr.mch.mdo.restaurant.services.business.managers.AbstractAdministrationManagerLabelable;
@@ -28,6 +53,13 @@ import fr.mch.mdo.utils.IManagerAssembler;
 
 public class DefaultProductsManager extends AbstractAdministrationManagerLabelable implements IProductsManager
 {
+	/**	rateFormat is used formatting Bigdecimal rate */
+	private NumberFormat vatRateFormat;
+	{
+		DecimalFormatSymbols dfs = DecimalFormatSymbols.getInstance();
+		dfs.setDecimalSeparator('.');
+		vatRateFormat = new DecimalFormat("#.00", dfs);
+	}
 	private IRestaurantsManager restaurantsManager;
 	private IValueAddedTaxesManager valueAddedTaxesManager;
 	private ICategoriesManager categoriesManager;
@@ -143,7 +175,7 @@ public class DefaultProductsManager extends AbstractAdministrationManagerLabelab
 	}
 
 	@Override
-	public List<IMdoDtoBean> getList(Long restaurantId, MdoUserContext userContext) throws MdoException {
+	public List<IMdoDtoBean> getList(Long restaurantId, MdoUserContext userContext) throws MdoBusinessException {
 		List<IMdoDtoBean> result = new ArrayList<IMdoDtoBean>();
 		
 		try {
@@ -163,13 +195,13 @@ public class DefaultProductsManager extends AbstractAdministrationManagerLabelab
 	public IMdoDtoBean update(IMdoDtoBean dtoBean, MdoUserContext userContext) throws MdoBusinessException {
 		Product daoBean = (Product) assembler.unmarshal(dtoBean);
 		try {
-			if (daoBean != null && daoBean.getId() != null) {
-				// dummy is just used for updating daoBean.getCategories()
-				Product dummy  = (Product) dao.findByPrimaryKey(daoBean.getId());
-				dummy.getCategories().clear();
-				dummy.getCategories().addAll(daoBean.getCategories());
-				daoBean.setCategories(dummy.getCategories());
-			}
+			// Deleting daoBean.getCategories() before inserting new ones
+			Set<ProductCategory> backup = new HashSet<ProductCategory>(daoBean.getCategories());
+			// Removing
+			daoBean.getCategories().clear();
+			dao.update(daoBean);
+			// Restoring
+			daoBean.getCategories().addAll(backup);
 			return assembler.marshal((IMdoDaoBean) dao.update(daoBean), userContext);
 		} catch (MdoException e) {
 			logger.error("message.error.administration.business.save", e);
@@ -179,12 +211,164 @@ public class DefaultProductsManager extends AbstractAdministrationManagerLabelab
 	
 	@Override
 	public IMdoDtoBean delete(IMdoDtoBean dtoBean, MdoUserContext userContext) throws MdoBusinessException {
-		// Load data
-		dtoBean = super.findByPrimaryKey(dtoBean.getId(), userContext);
-		// Delete categories before
-		this.update(dtoBean, userContext);
+		// No need to Delete categories before Deleting user because of hibernate mapping all-delete-orphan in collection
 		// Delete dto
 		return super.delete(dtoBean, userContext);
 	}
 
+	@Override
+	public ProductDto findByCode(String restaurantReference, String code, MdoUserContext userContext) throws MdoException {
+		ProductDto result = null;
+		IMdoDaoBean product = (IMdoDaoBean) ((IProductsDao) dao).findByCode(restaurantReference, code);
+		result = (ProductDto) assembler.marshal(product, userContext);
+		return result;
+	}
+
+	@Override
+	public void importData(File file, MdoUserContext userContext) throws MdoBusinessException {
+		try {
+			Pattern pattern = Pattern.compile(IProductsManager.IMPORT_DATA_FILE_NAME_PATTERN);
+			Matcher matcher = pattern.matcher(file.getName());
+			boolean matchFound = matcher.find();
+			String restaurantReference = null;
+			String language = null;
+			if (matchFound) {
+		        restaurantReference = matcher.group(1);
+		        language = matcher.group(2);
+			} else {
+				throw new MdoBusinessException("message.error.administration.business.products.restaurant.not.found", new Object[]{file});
+			}
+
+			RestaurantDto restaurant = null;
+			restaurant = (RestaurantDto) restaurantsManager.findByReference(restaurantReference, userContext);
+			
+			if (restaurant != null) {
+				LocaleDto locale = null;
+				locale = (LocaleDto) localesManager.findByLanguage(language, userContext);
+				
+				if (locale != null) {
+					List vats = valueAddedTaxesManager.findAll(userContext);
+					Map<String, ValueAddedTaxDto> vatsRateId = new HashMap<String, ValueAddedTaxDto>();
+					for (Iterator iterator = vats.iterator(); iterator.hasNext();) {
+						ValueAddedTaxDto vat = (ValueAddedTaxDto) iterator.next();
+						// The vat rate from database is never null
+						// If the vat rate is not unique then keep the last one
+						vatsRateId.put(vatRateFormat.format(vat.getRate().doubleValue()), vat);
+					}
+
+					// Load the file.
+					Sheet sheet = SpreadSheet.createFromFile(file).getSheet(0);
+					logger.debug("Number of import rows " + sheet.getRowCount());
+					logger.debug("Number of import columns " + sheet.getColumnCount());
+					
+					int maxRow = sheet.getRowCount();
+					int maxColumn = sheet.getColumnCount();
+					for (int i = 1; i < maxRow; i++) {
+						ProductDto product = new ProductDto();
+						for (int j = 0; j < maxColumn; j++) {
+							Object cellValue = sheet.getCellAt(j, i).getValue();
+							logger.debug("Cell at column " + j + " and row " + i + " is instance of " + (cellValue!=null?cellValue.getClass():"") + " with value " + cellValue);
+							// Fill product fields like Code, Label, Price ... See ProductRowTable.java
+							ProductRowTable.values()[j].fillValue(product, cellValue);
+						}
+						
+						if (product.getCode() != null && !product.getCode().isEmpty()) {
+							// labels(see ProductRowTable.LABEL.fillValue) has size one
+							Map<Long, String> labels = product.getLabels();
+							// labels is empty now
+							String label = labels.remove(null);
+							// labels has one size
+							labels.put(locale.getId(), label);
+	
+							// Product from Database
+							ProductDto productFromDatabase = this.findByCode(restaurant.getReference(), product.getCode(), userContext);
+							if (productFromDatabase != null) {
+								// Here, set Values that are not in imported file
+								product.setId(productFromDatabase.getId());
+								product.setCategories(productFromDatabase.getCategories());
+								
+								if (productFromDatabase.getLabels() != null) {
+									labels.putAll(productFromDatabase.getLabels());
+								}
+							}
+							
+							// Restaurant
+							product.setRestaurant(restaurant);
+							
+							// VAT: see ProductRowTable.VAT.fillValue
+							BigDecimal rate = product.getVat().getRate();
+							if (rate != null) {
+								String formattedRate = vatRateFormat.format(rate.doubleValue());
+								ValueAddedTaxDto vat = vatsRateId.get(formattedRate);
+								if (vat != null) {
+									product.setVat(vat);
+								}
+							} 
+							if (product.getVat() == null) {
+								throw new MdoBusinessException("message.error.administration.business.products.vat.not.found", new Object[]{file});
+							}
+							
+							// Save into database
+							super.save(product, userContext);
+						} else {
+							// Considerer that is no more row to import in the imported file
+							break;
+						}
+					}
+				}
+			} else {
+				throw new MdoBusinessException("message.error.administration.business.products.restaurant.not.found", new Object[]{file});
+			}
+		} catch (Exception e) {
+			throw new MdoBusinessException("message.error.administration.business.products.restaurant.not.found", new Object[]{file});
+		}
+	}
+
+	@Override
+	public void exportData(OutputStream out, String restaurantReference, String[] headers, MdoUserContext userContext) throws MdoBusinessException {
+		RestaurantDto restaurant;
+		try {
+			restaurant = (RestaurantDto) this.getRestaurantsManager().findByReference(restaurantReference, userContext);
+		} catch (MdoException e) {
+			throw new MdoBusinessException(e);
+		}
+
+		if (restaurant == null) {
+			throw new MdoBusinessException("message.error.administration.business.products.restaurant.not.found", new Object[]{restaurantReference});
+		}
+		
+		Long localeId = userContext.getCurrentLocale().getId();
+		List<IMdoDtoBean> list = this.getList(restaurant.getId(), userContext);
+		int indexRow = 0;
+		// Create the data to export: 0 row, 5 columns by default
+		Object[][] data = new Object[0][5];
+		if (list != null) {
+			data = new Object[list.size()][5];
+			for (IMdoDtoBean iMdoDtoBean : list) {
+				ProductDto castedBean = (ProductDto) iMdoDtoBean;
+				data[indexRow++] = new Object[] { castedBean.getCode(), castedBean.getLabels().get(localeId), castedBean.getPrice(), castedBean.getVat().getRate(), castedBean.getColorRGB() };	
+				logger.debug("Row index = " + indexRow 
+						+ ", Code = " + castedBean.getCode() + ", Label = " + castedBean.getLabels().get(localeId) 
+						+ ", Price = " +  castedBean.getPrice() + ", Vat = " +  castedBean.getVat().getRate() 
+						+ ", Color = " +  castedBean.getColorRGB());
+			}
+		}
+		logger.debug("Rows Size = " + data.length + " - Columns Size = " + (data.length==0?0:data[0].length));
+		
+		TableModel model = new DefaultTableModel(data, headers);
+
+		try {
+//			SpreadSheet.createEmpty(model).saveAs(file);
+			SpreadSheet.createEmpty(model).getPackage().save(out);
+			
+		} catch (IOException e) {
+			throw new MdoBusinessException("message.error.administration.business.products.export.data", new Object[]{restaurantReference});
+		}
+	}
+	
+	public static void main(String[] args) {
+		Map m = new HashMap();
+		m.put(null, null);
+		System.out.println(m.get(null));
+	}
 }
