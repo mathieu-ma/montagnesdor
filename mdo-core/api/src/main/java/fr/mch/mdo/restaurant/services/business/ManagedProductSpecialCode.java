48f5b8d744d43e27c6cf17f5815d5c7ad01e3872
Mathieu MA <mathieu.ma.dev@free.fr>
2011-06-05 21:17:04 +0000

    Initial commit from Ubuntu

--- mdo-core/api/src/main/java/fr/mch/mdo/restaurant/services/business/ManagedProductSpecialCode.java
+++ mdo-core/api/src/main/java/fr/mch/mdo/restaurant/services/business/ManagedProductSpecialCode.java
@@ -2,7 +2,7 @@ package fr.mch.mdo.restaurant.services.business;
 
 public enum ManagedProductSpecialCode 
 {
-	NOTHING(""), OFFERED_PRODUCT("#"), REDUCED_ORDER("-"), USER_ORDER("/");
+	NOTHING(""), OFFERED_PRODUCT("#"), DISCOUNT_ORDER("-"), USER_ORDER("/");
 	private String shortCode = null;
 
 	private ManagedProductSpecialCode(String shortCode) {
