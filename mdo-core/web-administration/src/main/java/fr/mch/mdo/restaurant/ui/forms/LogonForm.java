91cc514de75c8a536e16b44d2352b34959b26ef5
Mathieu MA <mathieu.ma.dev@free.fr>
2011-06-13 09:12:35 +0000

    Initial commit from Ubuntu

--- mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/LogonForm.java
+++ mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/LogonForm.java
@@ -1,13 +1,10 @@
 package fr.mch.mdo.restaurant.ui.forms;
 
-import org.apache.commons.beanutils.BeanUtils;
-
 import fr.mch.mdo.restaurant.dto.beans.LogonDto;
 
 public class LogonForm extends MdoForm
 {
-    public LogonForm()
-    {
-	super(new LogonDto());
-    }
+	public LogonForm() {
+		super(new LogonDto());
+	}
 }
--- mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/ProductsManagerForm.java
+++ mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/ProductsManagerForm.java
@@ -1,12 +1,92 @@
 package fr.mch.mdo.restaurant.ui.forms;
 
+import java.io.File;
+import java.io.InputStream;
+
 import fr.mch.mdo.restaurant.dto.beans.ProductDto;
 import fr.mch.mdo.restaurant.dto.beans.ProductsManagerViewBean;
 
 public class ProductsManagerForm extends MdoLabelsForm 
 {
+	private InputStream exportInputStream;
+	private String exportFileName;
+	
+	private File importedFile;
+	private String importedFileContentType;
+	private String importedFileFileName;
+	
 	public ProductsManagerForm() {
 		super(new ProductDto());
 		super.setViewBean(new ProductsManagerViewBean());
+	}
+
+	/**
+	 * @return the exportInputStream
+	 */
+	public InputStream getExportInputStream() {
+		return exportInputStream;
+	}
+
+	/**
+	 * @param exportInputStream the exportInputStream to set
+	 */
+	public void setExportInputStream(InputStream exportInputStream) {
+		this.exportInputStream = exportInputStream;
+	}
+
+	/**
+	 * @return the exportFileName
+	 */
+	public String getExportFileName() {
+		return exportFileName;
+	}
+
+	/**
+	 * @param exportFileName the exportFileName to set
+	 */
+	public void setExportFileName(String exportFileName) {
+		this.exportFileName = exportFileName;
+	}
+
+	/**
+	 * @param importedFile the importedFile to set
+	 */
+	public void setImportedFile(File importedFile) {
+		this.importedFile = importedFile;
+	}
+
+	/**
+	 * @return the importedFile
+	 */
+	public File getImportedFile() {
+		return importedFile;
+	}
+
+	/**
+	 * @return the importedFileContentType
+	 */
+	public String getImportedFileContentType() {
+		return importedFileContentType;
+	}
+
+	/**
+	 * @param importedFileContentType the importedFileContentType to set
+	 */
+	public void setImportedFileContentType(String importedFileContentType) {
+		this.importedFileContentType = importedFileContentType;
+	}
+
+	/**
+	 * @return the importedFileFileName
+	 */
+	public String getImportedFileFileName() {
+		return importedFileFileName;
+	}
+
+	/**
+	 * @param importedFileFileName the importedFileFileName to set
+	 */
+	public void setImportedFileFileName(String importedFileFileName) {
+		this.importedFileFileName = importedFileFileName;
 	}
 }
