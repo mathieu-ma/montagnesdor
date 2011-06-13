15f6843eb38fe0cfb4eb3f6c785ff9873bad88f2
Mathieu MA <mathieu.ma.dev@free.fr>
2011-06-13 09:08:30 +0000

    Initial commit from Ubuntu

--- mdo-core/dao/src/main/java/fr/mch/mdo/restaurant/dao/products/hibernate/DefaultProductSoldsDao.java
+++ mdo-core/dao/src/main/java/fr/mch/mdo/restaurant/dao/products/hibernate/DefaultProductSoldsDao.java
@@ -1,14 +1,19 @@
 package fr.mch.mdo.restaurant.dao.products.hibernate;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.hibernate.Session;
 
 import fr.mch.mdo.logs.ILogger;
 import fr.mch.mdo.restaurant.beans.IMdoBean;
 import fr.mch.mdo.restaurant.beans.IMdoDaoBean;
 import fr.mch.mdo.restaurant.dao.beans.ProductSold;
 import fr.mch.mdo.restaurant.dao.hibernate.DefaultDaoServices;
+import fr.mch.mdo.restaurant.dao.hibernate.TransactionSession;
 import fr.mch.mdo.restaurant.dao.products.IProductSoldsDao;
 import fr.mch.mdo.restaurant.exception.MdoDataBeanException;
 import fr.mch.mdo.restaurant.services.logs.LoggerServiceImpl;
@@ -52,5 +57,18 @@ public class DefaultProductSoldsDao extends DefaultDaoServices implements IProdu
 	@Override
 	public IMdoBean findByUniqueKey(Date soldDate, Long productId) throws MdoDataBeanException {
 		return this.findByUniqueKey(new Object[] { soldDate, productId });
+	}
+	
+	public List<ProductSold> findByYear(int year) {
+		List<ProductSold> result = new ArrayList<ProductSold>();
+		TransactionSession transactionSession = super.beginTransaction();
+
+		Session session = transactionSession.getSession();
+		result = session.createQuery("FROM ProductSold pds WHERE extract(year FROM pds.soldDate)="+year).list();
+		
+
+		super.endTransaction(transactionSession, result, true);
+
+		return result;
 	}
 }
