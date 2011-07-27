f3cd4735ac6aaf8888e23c8b8acf0b892db2afe7
Mathieu MA <mathieu.ma.dev@free.fr>
2011-07-27 06:27:06 +0000

    Initial commit from Ubuntu

--- mdo-core/ioc/src/test/java/fr/mch/mdo/restaurant/ioc/WebAdministrationBeanFactoryTest.java
+++ mdo-core/ioc/src/test/java/fr/mch/mdo/restaurant/ioc/WebAdministrationBeanFactoryTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestSuite;
 import fr.mch.mdo.logs.ILogger;
 import fr.mch.mdo.restaurant.authentication.IMdoAuthenticationService;
 import fr.mch.mdo.restaurant.ioc.spring.MdoBeanFactory;
-import fr.mch.mdo.restaurant.ioc.spring.WebAdministractionBeanFactory;
+import fr.mch.mdo.restaurant.ioc.spring.WebAdministrationBeanFactory;
 import fr.mch.mdo.restaurant.services.business.managers.ICategoriesManager;
 import fr.mch.mdo.restaurant.services.business.managers.locales.ILocalesManager;
 import fr.mch.mdo.restaurant.services.business.managers.printings.IPrintingInformationsManager;
@@ -38,109 +38,109 @@ public class WebAdministrationBeanFactoryTest extends MdoIocBasicTestCase
 	}
 
 	public void testGetInstance() {
-		IWebAdministractionBeanFactory iWebAdministractionBeanFactory = WebAdministractionBeanFactory.getInstance();
-		assertTrue(iWebAdministractionBeanFactory instanceof WebAdministractionBeanFactory);
+		IWebAdministrationBeanFactory iWebAdministrationBeanFactory = WebAdministrationBeanFactory.getInstance();
+		assertTrue(iWebAdministrationBeanFactory instanceof WebAdministrationBeanFactory);
 
 		IMdoBeanFactory iMdoBeanFactory = MdoBeanFactory.getInstance();
 		assertTrue(iMdoBeanFactory instanceof MdoBeanFactory);
 
 		// The following bean is singleton scope
-		assertTrue("Check reference equality of IOC beans", iWebAdministractionBeanFactory.getMessageQuery() == iMdoBeanFactory.getMessageQuery());
+		assertTrue("Check reference equality of IOC beans", iWebAdministrationBeanFactory.getMessageQuery() == iMdoBeanFactory.getMessageQuery());
 	}
 
 	public void testGetLogger() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getLogger();
 		assertNotNull("ILogger not null", manager);
 		assertTrue("Check ILogger", manager instanceof ILogger);
 	}
 
 	public void testGetMdoAuthorizationService() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getMdoAuthenticationService();
 		assertNotNull("IMdoAuthenticationService not null", manager);
 		assertTrue("Check IMdoAuthenticationService", manager instanceof IMdoAuthenticationService);
 	}
 
 	public void testGetMdoAuthenticationService() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getMdoAuthenticationService();
 		assertNotNull("IMdoAuthenticationService not null", manager);
 		assertTrue("Check IMdoAuthenticationService", manager instanceof IMdoAuthenticationService);
 	}
 
 	public void testGetMdoTableAsEnumsManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getMdoTableAsEnumsManager();
 		assertNotNull("IMdoTableAsEnumsManager not null", manager);
 		assertTrue("Check IMdoTableAsEnumsManager", manager instanceof IMdoTableAsEnumsManager);
 	}
 
 	public void testGetLocalesManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getLocalesManager();
 		assertNotNull("ILocalesManager not null", manager);
 		assertTrue("Check ILocalesManager", manager instanceof ILocalesManager);
 	}
 
 	public void testGetRestaurantsManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getRestaurantsManager();
 		assertNotNull("IRestaurantsManager not null", manager);
 		assertTrue("Check IRestaurantsManager", manager instanceof IRestaurantsManager);
 	}
 
 	public void testGetUsersManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getUsersManager();
 		assertNotNull("IUsersManager not null", manager);
 		assertTrue("Check IUsersManager", manager instanceof IUsersManager);
 	}
 
 	public void testGetUserRolesManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getUserRolesManager();
 		assertNotNull("IUserRolesManager not null", manager);
 		assertTrue("Check IUsersIUserRolesManagerManager", manager instanceof IUserRolesManager);
 	}
 
 	public void testGetUserAuthenticationsManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getUserAuthenticationsManager();
 		assertNotNull("IUserAuthenticationsManager not null", manager);
 		assertTrue("Check IUserAuthenticationsManager", manager instanceof IUserAuthenticationsManager);
 	}
 
 	public void testGetProductsManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getProductsManager();
 		assertNotNull("IProductsManager not null", manager);
 		assertTrue("Check IProductsManager", manager instanceof IProductsManager);
 	}
 
 	public void testGetCategoriesManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getCategoriesManager();
 		assertNotNull("ICategoriesManager not null", manager);
 		assertTrue("Check ICategoriesManager", manager instanceof ICategoriesManager);
 	}
 
 	public void testGetValueAddedTaxesManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getValueAddedTaxesManager();
 		assertNotNull("IValueAddedTaxesManager not null", manager);
 		assertTrue("Check IValueAddedTaxesManager", manager instanceof IValueAddedTaxesManager);
 	}
 
 	public void testGetProductSpecialCodesManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getProductSpecialCodesManager();
 		assertNotNull("IProductSpecialCodesManager not null", manager);
 		assertTrue("Check IProductSpecialCodesManager", manager instanceof IProductSpecialCodesManager);
 	}
 
 	public void testGetPrintingInformationsManager() {
-		IWebAdministractionBeanFactory iMdoBeanFactory = WebAdministractionBeanFactory.getInstance();
+		IWebAdministrationBeanFactory iMdoBeanFactory = WebAdministrationBeanFactory.getInstance();
 		Object manager = iMdoBeanFactory.getPrintingInformationsManager();
 		assertNotNull("IPrintingInformationsManager not null", manager);
 		assertTrue("Check IPrintingInformationsManager", manager instanceof IPrintingInformationsManager);
