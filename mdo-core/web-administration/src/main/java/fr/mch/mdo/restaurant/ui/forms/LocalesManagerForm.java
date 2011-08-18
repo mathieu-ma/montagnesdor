d0b8866355b4134e7cf5f1a968f01d822bd77788
Mathieu MA <mathieu.ma.dev@free.fr>
2011-08-18 05:08:08 +0000

    Initial commit from Ubuntu

--- mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/LocalesManagerForm.java
+++ mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/ui/forms/LocalesManagerForm.java
@@ -3,7 +3,7 @@ package fr.mch.mdo.restaurant.ui.forms;
 import fr.mch.mdo.restaurant.dto.beans.LocaleDto;
 import fr.mch.mdo.restaurant.dto.beans.LocalesManagerViewBean;
 
-public class LocalesManagerForm extends MdoForm 
+public class LocalesManagerForm extends MdoAdministrationForm 
 {
 	public LocalesManagerForm() {
 		super(new LocaleDto());
