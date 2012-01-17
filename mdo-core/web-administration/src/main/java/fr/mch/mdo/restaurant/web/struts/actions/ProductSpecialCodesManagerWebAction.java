8481c0cdbf253ce9f3343531a02647bd4ff6b69f
Mathieu MA <mathieu.ma.dev@free.fr>
2012-01-17 22:09:26 +0000

    Update Code

--- mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/web/struts/actions/ProductSpecialCodesManagerWebAction.java
+++ mdo-core/web-administration/src/main/java/fr/mch/mdo/restaurant/web/struts/actions/ProductSpecialCodesManagerWebAction.java
@@ -1,6 +1,17 @@
 package fr.mch.mdo.restaurant.web.struts.actions;
 
+import fr.mch.mdo.restaurant.Constants;
+import fr.mch.mdo.restaurant.dto.beans.IAdministrationManagerViewBean;
+import fr.mch.mdo.restaurant.dto.beans.MdoUserContext;
+import fr.mch.mdo.restaurant.dto.beans.ProductDto;
+import fr.mch.mdo.restaurant.dto.beans.ProductSpecialCodeDto;
+import fr.mch.mdo.restaurant.dto.beans.RestaurantDto;
+import fr.mch.mdo.restaurant.exception.MdoException;
 import fr.mch.mdo.restaurant.ioc.spring.WebAdministrationBeanFactory;
+import fr.mch.mdo.restaurant.services.business.managers.products.IProductSpecialCodesManager;
+import fr.mch.mdo.restaurant.services.business.managers.products.IProductsManager;
+import fr.mch.mdo.restaurant.services.business.managers.restaurants.IRestaurantsManager;
+import fr.mch.mdo.restaurant.ui.forms.IMdoAdministrationForm;
 import fr.mch.mdo.restaurant.ui.forms.ProductSpecialCodesManagerForm;
 
 public class ProductSpecialCodesManagerWebAction extends AdministrationManagerLabelsAction 
@@ -10,8 +21,63 @@ public class ProductSpecialCodesManagerWebAction extends AdministrationManagerLa
      */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Restaurants Manager
+	 */
+	private IRestaurantsManager restaurantsManager;
+
 	public ProductSpecialCodesManagerWebAction() {
 		super(WebAdministrationBeanFactory.getInstance().getLogger(ProductSpecialCodesManagerWebAction.class.getName()), new ProductSpecialCodesManagerForm());
 		administrationManager = WebAdministrationBeanFactory.getInstance().getProductSpecialCodesManager();
+		restaurantsManager = WebAdministrationBeanFactory.getInstance().getRestaurantsManager();
 	}
+	
+	/**
+	 * @return the restaurantsManager
+	 */
+	public IRestaurantsManager getRestaurantsManager() {
+		return restaurantsManager;
+	}
+
+	/**
+	 * @param restaurantsManager the restaurantsManager to set
+	 */
+	public void setRestaurantsManager(IRestaurantsManager restaurantsManager) {
+		this.restaurantsManager = restaurantsManager;
+	}
+
+	@Override
+	public String save() {
+		super.save();
+		
+		// Reload the restaurant bean
+		ProductSpecialCodeDto dtoBean = ((ProductSpecialCodeDto) super.getForm().getDtoBean());
+		RestaurantDto restaurant = dtoBean.getRestaurant();
+		try {
+			restaurant = (RestaurantDto) restaurantsManager.findByPrimaryKey(dtoBean.getRestaurant().getId(), (MdoUserContext) super.getForm().getUserContext(), false);
+		} catch (MdoException e) {
+			super.addActionError(super.getText("error.action.technical", new String[] {this.getClass().getName(), "save"}));
+		}
+		dtoBean.setRestaurant(restaurant);
+		// Return to the products list
+		return Constants.ACTION_RESULT_AFTER_CUD_LIST_PRODUCT_SPECIAL_CODES;
+
+	}
+	
+	public String listProductSpecialCodes() throws Exception {
+		String result = Constants.ACTION_RESULT_AFTER_SUCCESS_FORM_LIST;
+		ProductSpecialCodeDto dtoBean = ((ProductSpecialCodeDto) super.getForm().getDtoBean());
+		if (dtoBean != null) {
+			MdoUserContext userContext = (MdoUserContext) super.getForm().getUserContext();
+			if (userContext != null) {
+				IAdministrationManagerViewBean viewBean = ((IMdoAdministrationForm) super.getForm()).getViewBean();
+				if (viewBean != null && dtoBean.getRestaurant() != null) {
+					IProductSpecialCodesManager manager = (IProductSpecialCodesManager) administrationManager;
+					viewBean.setList(manager.getList(dtoBean.getRestaurant().getId(), userContext));
+				}
+			}
+		}
+		return result;
+	}
+
 }
